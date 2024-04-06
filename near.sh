7f7988f6def33e7cb456b296b96c6a15e4244abf
annisabalilaqute <153105259+annisabalilaqute@users.noreply.github.com>
2024-04-06 11:18:41 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://api.open-meteo.com/v1/metno?latitude=59.91&longitude=10.75&hourly=temperature_2m"
+RPC_URL="https://near.lava.build/lava-referer-40caf1a9-e762-4910-910c-9d3613e5e5f6/"
 
 OUTPUT_DIR="logs"
 
